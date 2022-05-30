774067cb694472b6b1f136538094e2d08055a7df
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-05-29 23:08:55 -0500

    first commit, mock database

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// const router = require('./components/message/network');
+const router = require('./network/routes');
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+// app.use(router);
+
+router(app)
+
+app.use('/app', express.static('public'));
+
+app.listen(3000);
+console.log('la aplicación está escuchando en http://localhost:3000')
